61dfab685b80b5f91e8030398cdf665be14e18f0
dpTablo <leeyw2355@gmail.com>
2023-05-05 12:46:04 +0900

    package.json - sass 의존성 추가
    css 을 scss 로 마이그레이션

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './globals.css';
+import './globals.scss';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Image from 'next/image';
 import { Inter } from '@next/font/google';
-import styles from './page.module.css';
+import styles from './page.module.scss';
 
 const inter = Inter({ subsets: ['latin'] });
 
